b7c1d581e7812a9eb04b9feb5229b435499e0aa8
imvickykumar999 <imvickykumar999@gmail.com>
2023-09-18 11:40:38 +0530

    live on pythonanywhere

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 
 import requests, random
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 
 app = Flask(__name__)
 
@@ -26,52 +26,7 @@ def skills():
 
 @app.route('/news')
 def news():
-    # https://newsapi.org/account
-    source = ['the-hindu', 'the-times-of-india', 'bbc-news', 'cnn', 
-              'the-verge', 'time', 'the-wall-street-journal', ]
-    
-    source = random.choice(source)
-    print(source)
-
-    try:
-        bot_token = '****************************'
-        gets = f'https://newsapi.org/v1/articles?source={source}&sortBy=top&apiKey={bot_token}'
-        
-        req = requests.get(gets) 
-        box = req.json()['articles']
-
-    except:
-        try:
-            bot_token = '******************************'
-            gets = f'https://newsapi.org/v1/articles?source={source}&sortBy=top&apiKey={bot_token}'
-            
-            req = requests.get(gets) 
-            box = req.json()['articles']
-
-        except:
-            bot_token = '*****************************'
-            gets = f'https://newsapi.org/v1/articles?source={source}&sortBy=top&apiKey={bot_token}'
-            
-            req = requests.get(gets) 
-            box = req.json()['articles']
-
-    ha,ia,ba,la = [],[],[],[]
-    for i in range(len(box)):
-        h = box[i]['title']
-        m = box[i]['urlToImage']
-        b = box[i]['description']
-        l='link not found'
-
-        try:
-            l = box[i]['url']
-        except:
-            pass
-
-        ha.append(h)
-        ia.append(m)
-        ba.append(b)
-        la.append(l)
-    return render_template('news.html', ha=ha, ia=ia, ba=ba, la=la, len = len(ha))
+    return redirect('https://imvickykumar999.pythonanywhere.com/')
 
 @app.errorhandler(404)
 def page_not_found(e):
