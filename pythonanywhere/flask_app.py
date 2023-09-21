4b5c6c7ac09bee20b359ffbac1f60fb580f12eef
imvickykumar999 <imvickykumar999@gmail.com>
2023-09-21 15:21:42 +0530

    nav bar linked

--- pythonanywhere/flask_app.py
+++ pythonanywhere/flask_app.py
@@ -2,8 +2,12 @@
 import requests, random
 from flask import Flask, render_template
 from bs4 import BeautifulSoup as bs
+from stem.control import Controller
 
 app = Flask(__name__)
+port = 5000
+host = "127.0.0.1"
+hidden_svc_dir = "C:/Users/Vicky/Desktop/Repository/Host-Onion/Tor Browser/sample_flask"
 
 def get_news(source):
     if source == 'inshorts':
@@ -109,4 +113,22 @@ def page_not_found(e):
     return render_template('404.html'), 404
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", debug=True)
+    print (" * Getting controller")
+    controller = Controller.from_port(address=host, port=9151)
+
+    try:
+        controller.authenticate(password="")
+        controller.set_options([
+            ("HiddenServiceDir", hidden_svc_dir),
+            ("HiddenServicePort", "80 %s:%s" % (host, str(port)))
+            ])
+        svc_name = open(hidden_svc_dir + "/hostname", "r").read().strip()
+        print (" * Created host: %s" % svc_name)
+
+    except Exception as e:
+        print (e)
+
+    app.run(
+        host="0.0.0.0", 
+        debug=True
+    )
