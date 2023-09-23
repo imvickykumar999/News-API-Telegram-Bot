a2f7173653c22daec27ec274adcc68460367d5d5
imvickykumar999 <imvickykumar999@gmail.com>
2023-09-23 12:24:44 +0530

    zipped

--- pythonanywhere/flask_app.py
+++ pythonanywhere/flask_app.py
@@ -13,6 +13,8 @@ import requests, random
 from flask import Flask, render_template
 from bs4 import BeautifulSoup as bs
 
+from HostTor import VicksTor
+import VicksTor
 app = Flask(__name__)
 
 def get_news(source):
@@ -120,30 +122,7 @@ def page_not_found(e):
     return render_template('404.html'), 404
 
 if __name__ == '__main__':
-
-    ## To uncomment, Open Tor Browser
-    # from stem.control import Controller
-
-    # port = 5000
-    # host = "127.0.0.1"
-    # hidden_svc_dir = "C:/Users/Vicky/Desktop/Repository/Host-Onion/Tor Browser/sample_flask"
-
-    # print (" * Getting controller")
-    # controller = Controller.from_port(address=host, port=9151)
-
-    # try:
-    #     controller.authenticate(password="")
-    #     controller.set_options([
-    #         ("HiddenServiceDir", hidden_svc_dir),
-    #         ("HiddenServicePort", "80 %s:%s" % (host, str(port)))
-    #         ])
-    #     svc_name = open(hidden_svc_dir + "/hostname", "r").read().strip()
-    #     print (" * Created host: %s" % svc_name)
-
-    # except Exception as e:
-    #     print (e)
-
     app.run(
-        host="0.0.0.0", 
-        debug=True
+        # host="0.0.0.0", 
+        debug=False
     )
